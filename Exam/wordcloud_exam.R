9f16954741175ead072fc120f5ed9a9426da51d3
FisherFemboy <145297939+FisherFemboy@users.noreply.github.com>
2025-12-12 19:12:31 +0800

    Add files via upload

---
+++ Exam/wordcloud_exam.R
@@ -0,0 +1,11 @@
+# setwd("C:/Users/Helloworld/OneDrive/Desktop/Exam")
+# 
+# library(tm)
+# library(SnowballC)
+# library(wordcloud)
+# library(RColorBrewer)
+
+set.seed(123)
+nouns <- c("service", "queue", "staff", "clerk", "process", "officer", "license", "documents", "system", "waiting")
+actions <- c("was", "is", "rated", "considered", "found")
+ratings <- c("satisfactory", "unsatisfactory", "highly satisfactory", "poor", "average", "outstanding", "acceptable", "disappointing")
